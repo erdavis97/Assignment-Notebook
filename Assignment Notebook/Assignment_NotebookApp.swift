51ed035a820b85ab0e7f5e7f25e125163c1a8224
Ethan Davis <edavis@bsd220.org>
2024-01-29 09:55:54 -0600

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Ethan Davis on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
